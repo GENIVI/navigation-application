8e11a671160ed2fcba8558ebf9e79fc61e29a67e
Martin Schaller <martin.schaller@it-schaller.de>
2014-05-16 01:20:14 -0700

    Improved fuel stop advisor

--- src/fuel-stop-advisor/fuel-stop-advisor.cpp
+++ src/fuel-stop-advisor/fuel-stop-advisor.cpp
@@ -5,6 +5,8 @@
 #include "fuel-stop-advisor.h"
 #include "constants.h"
 #include "amb.h"
+#include "genivi-navigationcore-constants.h"
+#include "genivi-navigationcore-routing_proxy.h"
 
 static DBus::Glib::BusDispatcher dispatcher;
 static DBus::Connection *conn;
@@ -14,7 +16,23 @@ static GMainLoop *loop;
 #define TRIP_COUNT 2
 
 /* vehicle parameter */
-static double fuel_consumption_l_100km=7.0;
+static double fuel_consumption_l_100km=6.3;
+
+static double fuel_consumption_speed[]={
+	6.0, /* 0-9 km/h */
+	6.0, /* 10-19 km/h */
+	6.0, /* 20-29 km/h */
+	6.0, /* 30-39 km/h */
+	4.5, /* 40-49 km/h */
+	4.5, /* 50-59 km/h */
+	4.5, /* 60-69 km/h */
+	5.5, /* 70-79 km/h */
+	5.5, /* 80-89 km/h */
+	7.0, /* 90-99 km/h */
+	7.0, /* 100-109 km/h */
+	7.0, /* 110-119 km/h */
+	7.0, /* 120-129 km/h */
+};
 
 
 static DBus::Variant
@@ -60,6 +78,41 @@ class Properties
 		::DBus::Variant argout;
 		ri >> argout;
 		return argout;
+	}
+};
+
+class Routing
+: public org::genivi::navigationcore::Routing_proxy,
+  public DBus::ObjectProxy
+{
+	public:
+	Routing(DBus::Connection &connection)
+        : DBus::ObjectProxy(connection, "/org/genivi/navigationcore","org.genivi.navigationcore.Routing")
+	{
+	}
+
+	void RouteDeleted(const uint32_t& routeHandle)
+	{
+	}
+
+	void RouteCalculationCancelled(const uint32_t& routeHandle)
+	{
+	}
+
+	void RouteCalculationSuccessful(const uint32_t& routeHandle, const std::map< uint16_t, uint16_t >& unfullfilledPreferences)
+	{
+	}
+
+	void RouteCalculationFailed(const uint32_t& routeHandle, const uint16_t& errorCode, const std::map< uint16_t, uint16_t >& unfullfilledPreferences)
+	{
+	}
+
+	void RouteCalculationProgressUpdate(const uint32_t& routeHandle, const uint16_t& status, const uint8_t& percentage)
+	{
+	}
+
+	void AlternativeRoutesAvailable(const std::vector< uint32_t >& routeHandlesList)
+	{
 	}
 };
 
@@ -142,12 +195,20 @@ class FuelStopAdvisor
 		amb_conn->setup(&amb_dispatcher);
 		amb=new AutomotiveMessageBroker(*amb_conn);
 
+		routing_dispatcher.attach(NULL);
+		routing_conn = new DBus::Connection(DBus::Connection::SessionBus());
+		routing_conn->setup(&routing_dispatcher);
+		routing=new Routing(*routing_conn);
+
 		fuel = amb->FindObject("Fuel");
 		odometer = amb->FindObject("Odometer");
 		printf("%d %d\n",fuel.size(), odometer.size());
 
 		fuel_properties=new Properties(*amb_conn,fuel[0]);
 		odometer_properties=new Properties(*amb_conn,odometer[0]);
+		advisorMode=false;
+		distanceThreshold=0;
+		routeHandle=0;
         }
 
 	::DBus::Struct< uint16_t, uint16_t, uint16_t, std::string >
@@ -173,6 +234,7 @@ class FuelStopAdvisor
 		uint32_t odometer;
 		uint16_t level;
 		uint16_t consumption;
+		double remaining;
 		DBus::Variant variant;
 		DBus::MessageIter it;
 
@@ -192,6 +254,7 @@ class FuelStopAdvisor
 		ret[GENIVI_FUELSTOPADVISOR_INSTANT_FUEL_CONSUMPTION_PER_DISTANCE]=variant_uint16(consumption);
 
 		ret[GENIVI_FUELSTOPADVISOR_TANK_DISTANCE]=variant_uint16(level/fuel_consumption_l_100km*100+0.5);
+		ret[GENIVI_FUELSTOPADVISOR_ENHANCED_TANK_DISTANCE]=variant_uint16(enhancedDistance(level,remaining)+0.5);
 		return ret;
 	}
 
@@ -241,6 +304,51 @@ class FuelStopAdvisor
 		trips[number].update(fuel_properties, odometer_properties);
 		TripDataResetted(number);
 	}
+
+	double fuelConsumptionAtSpeed(uint16_t seg_speed)
+	{
+		if (seg_speed > 129)
+			seg_speed=129;
+		return fuel_consumption_speed[seg_speed/10];
+	}
+
+	double enhancedDistance(double level, double &remaining)
+	{
+		double distance=0;
+		printf("routeHandle %d\n",routeHandle);
+		if (routeHandle) {
+			std::vector< std::map< uint16_t, ::DBus::Variant > > RouteShape;
+			std::vector< uint16_t > valuesToReturn;
+			uint32_t totalNumberOfSegments;
+			valuesToReturn.push_back(GENIVI_NAVIGATIONCORE_DISTANCE);
+			valuesToReturn.push_back(GENIVI_NAVIGATIONCORE_SPEED);
+			routing->GetRouteSegments(routeHandle, 1, valuesToReturn, 0xffffffff, 0, totalNumberOfSegments, RouteShape);
+			for (int i=0 ; i < RouteShape.size(); i++) {
+				double seg_distance;
+				uint16_t seg_speed;
+				DBus::MessageIter it;
+
+				it=RouteShape[i][GENIVI_NAVIGATIONCORE_DISTANCE].reader();
+				it >> seg_distance;
+				it=RouteShape[i][GENIVI_NAVIGATIONCORE_SPEED].reader();
+				it >> seg_speed;
+				if (seg_distance && seg_speed) {
+					double fuel_consumption=fuelConsumptionAtSpeed(seg_speed)*seg_distance/100000;
+					if (fuel_consumption > level && level > 0) {
+						seg_distance=seg_distance*level/fuel_consumption;
+						fuel_consumption=level;
+					}
+					if (level > 0) 
+						distance+=seg_distance;
+					level-=fuel_consumption;
+				}
+			}
+			printf("%d segments\n",totalNumberOfSegments);
+		}
+		remaining=level/fuel_consumption_l_100km*100;
+		printf("distance_on_route %f remaining %f\n",distance/1000,remaining);
+		return distance/1000+(remaining > 0 ? remaining:0);
+	}
 	
 	void update_data()
 	{
@@ -248,47 +356,56 @@ class FuelStopAdvisor
 		for (i = 0 ; i < TRIP_COUNT ; i++) {
 			trips[i].update(fuel_properties, odometer_properties);
 		}
-#if 0
-		DBus::Variant variant=fuel_properties->Get("org.automotive.Fuel","Level");
-		DBus::MessageIter it=variant.reader();
-		uint16_t res;
-		it >> res;
-		printf("%d\n",res);
-		variant=fuel_properties->Get("org.automotive.Fuel","InstantConsumption");
-		it=variant.reader();
-		it >> res;
-		printf("%d\n",res);
-		variant=odometer_properties->Get("org.automotive.Odometer","Odometer");
-		it=variant.reader();
-		uint32_t res2;
-		it >> res2;
-		printf("%d\n",res2);
-#endif
+		if (advisorMode) {
+			uint16_t level;
+			double remaining;
+			DBus::Variant variant;
+			DBus::MessageIter it;
+
+			variant=fuel_properties->Get("org.automotive.Fuel","Level");
+			it=variant.reader();
+			it >> level;
+			enhancedDistance(level, remaining);
+			if (remaining < distanceThreshold)
+				FuelStopAdvisorWarning();
+		}
 	}
 
 	void
 	SetFuelAdvisorSettings(const bool& advisorMode, const uint8_t& distanceThreshold)
 	{
+		this->advisorMode=advisorMode;
+		this->distanceThreshold=distanceThreshold;
 	}
 	
 	void
 	GetFuelAdvisorSettings(bool& advisorMode, uint8_t& distanceThreshold)
 	{
+		advisorMode=this->advisorMode;
+		distanceThreshold=this->distanceThreshold;
 	}
 
 	void SetRouteHandle(const uint32_t& routeHandle)
 	{
+		printf("SetRouteHandle %d\n",routeHandle);
+		this->routeHandle=routeHandle;
 	}
 
 	private:
 	DBus::Glib::BusDispatcher amb_dispatcher;
 	DBus::Connection *amb_conn;
 	AutomotiveMessageBroker *amb;
+	DBus::Glib::BusDispatcher routing_dispatcher;
+	DBus::Connection *routing_conn;
+	Routing *routing;
 	std::vector< ::DBus::Path > fuel;
 	std::vector< ::DBus::Path > odometer;
 	Properties *fuel_properties;
 	Properties *odometer_properties;
 	Trip trips[TRIP_COUNT];
+	bool advisorMode;
+	uint8_t distanceThreshold;
+	uint32_t routeHandle;
 };
 
 static gboolean
